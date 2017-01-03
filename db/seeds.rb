5f23ed29a8f75822966b41190db59c3f9a44479c
Andrew <andrewmbyrd@gmail.com>
2017-01-03 17:34:28 -0600

    added seeds to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'random_data'
+require 'faker'
+
+20.times do
+  User.create!(
+  email: Faker::Internet.email,
+  password: Faker::Name.name
+  )
+end
+
+users = User.all
+
+15.times do
+  Wiki.create!(
+  title: Faker::Superhero.name,
+  body: Faker::ChuckNorris.fact,
+  private: false,
+  user: users.sample
+  )
+end
+
+puts "Seeding finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wiki entries created"
